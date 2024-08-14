c04291f06c55db51e0f8ed8dd60a0ee529fb4c54
Amarjitshende <129750852+Amarjitshende@users.noreply.github.com>
2024-08-14 15:56:21 +0530

    Update LoginForm.jsx

--- src/component/LoginForm/LoginForm.jsx
+++ src/component/LoginForm/LoginForm.jsx
@@ -20,7 +20,7 @@ export default function LoginForm() {
       </div>
       <button type='submit'>Login</button>
       <div className="register">
-        <p>Dont have account? <a href="#">Register</a> </p>
+        <p>Dont have account? <a>Register</a> </p>
       </div>
 
       </form>
