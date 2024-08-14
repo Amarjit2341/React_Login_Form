4b05059032d7681f197ed5d325c1f69bfc44004c
Amarjitshende <129750852+Amarjitshende@users.noreply.github.com>
2024-08-14 16:01:21 +0530

    Update LoginForm.jsx

--- src/component/LoginForm/LoginForm.jsx
+++ src/component/LoginForm/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import './LoginForm.css';
 import { FaUser  } from "react-icons/fa";
 import { FaMobile } from "react-icons/fa6";
@@ -20,10 +20,10 @@ export default function LoginForm() {
       </div>
       <button type='submit'>Login</button>
       <div className="register">
-        <p>Dont have account? <a>Register</a> </p>
+        <p>Dont have account? <span>Register<span/> </p>
       </div>
 
       </form>
     </div>
-  )
+  );
 }
