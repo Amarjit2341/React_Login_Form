11a03c89deed39190a10a931e7ed00534b6bb199
Amarjit shahaji shende <amarjitshende94@gmail.com>
2024-08-19 15:19:57 +0530

    add commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import LoginForm from './component/LoginForm/LoginForm';
+import SignupForm from './component/SignupForm/SignupForm';
 function App() {
   return (
     <div>
-      <LoginForm/>
+      {/* <LoginForm/> */}
+      <SignupForm/>
     </div>
   );
 }
